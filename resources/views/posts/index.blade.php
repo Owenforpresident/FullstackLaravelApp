1a11f0a33806f6d2e2f85487644403320b3763b8
Owenforpresident <49999822+Owenforpresident@users.noreply.github.com>
2019-12-05 20:03:45 +0000

    I <3 Laravel!

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,16 @@
+@extends ('layouts.app')
+
+@section ('content')
+<h1 class= "card-title">Posts Archive</h1>
+@if(count($posts) >0)
+    @foreach($posts as $post)
+
+    <div class= "card" > 
+    <h3><a href="/posts/{{$post->id}}">{{$post->title}} </h3>
+<small>Written on {{$post->created_at}}</small>
+    </div>
+    @endforeach
+@else 
+<p> No Posts found!</p>
+@endif
+@endsection
